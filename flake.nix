cfeaa830240c9d65d5a6930569ecd065f45b79a3
khalil hadji <h-adji_tech@proton.me>
2026-09-12 21:32:52 +0200

    pytest

--- flake.nix
+++ flake.nix
@@ -2,35 +2,32 @@
   description = "CCraft nix shell";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs =
-    { nixpkgs, ... }:
-    let
-      devShellsFor =
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              gum
-              just
-              mdbook
-              just-lsp
-            ];
-            shellHook = ''
-              {
-                gum style --bold --foreground 214 "A CODE CRAFTER shapes software by hand :)"
-              } | gum style --border rounded --border-foreground 214 --padding "0 2" --margin "1 0"
-            '';
-          };
+    { nixpkgs, flake-utils, ... }:
 
-          # alternative shell profile : nix develop .#special
-          special = pkgs.mkShell { };
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gum
+            just
+            mdbook
+            just-lsp
+            uv
+          ];
+
+          shellHook = ''
+            {
+              gum style --bold --foreground 214 "A CODE CRAFTER shapes software by hand :)"
+            } | gum style --border rounded --border-foreground 214 --padding "0 2" --margin "1 0"
+          '';
         };
-    in
-    {
-      devShells = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-darwin" ] devShellsFor;
-    };
+      }
+    );
 }
