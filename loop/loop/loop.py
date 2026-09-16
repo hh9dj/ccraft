c0a2c7ac6294100f4f8bc42e07c3a10ef703b706
khalil hadji <h-adji_tech@proton.me>
2026-09-16 17:30:20 +0200

    event loop basic impl : call_soon + run_forever + stop

--- loop/loop/loop.py
+++ loop/loop/loop.py
@@ -1,2 +1,36 @@
+import collections
+import traceback
+
+
 class EventLoop:
-    pass
+    def __init__(self) -> None:
+        self._ready_queue = collections.deque()
+        self._running = False
+
+    def call_soon(self, cb, *args):
+        self._ready_queue.append((cb, args))
+
+    def run_forever(self):
+        if self._running:
+            raise RuntimeError("Loop already running")
+
+        self._running = True
+
+        try:
+            while len(self._ready_queue) and self._running:
+                cb, args = self._ready_queue.popleft()
+                try:
+                    cb(*args)
+                except Exception as e:
+                    traceback.print_exception(e)
+
+        finally:
+            self._running = False
+
+    def stop(self):
+        if self._running:
+            # NOTE: stop drops work from the queue (if running)
+            # maybe drain later to keep parity with
+            # reference implementation of asyncio loop
+            self._ready_queue.clear()
+        self._running = False
--- loop/main.py
+++ loop/main.py
@@ -1,4 +1,15 @@
 from loop import EventLoop
 
 if __name__ == "__main__":
-    EventLoop()
+    loop = EventLoop()
+
+    def test():
+        print("testing")
+        loop.run_forever()
+
+    loop.call_soon(print, "hello you")
+    loop.call_soon(test)
+    loop.call_soon(print, "this is your event loop :)")
+    loop.run_forever()
+
+    print("done :(")
